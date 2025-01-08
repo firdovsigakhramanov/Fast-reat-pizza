b39f27f6df8e4a430c0da4025c44460cae26e756
Firdovsi <firdovsigakhramanov@gmail.com>
2025-01-08 18:44:09 +0400

    tailwind contiues

---
+++ src/features/user/UserName.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function UserName() {
+  return (
+    <div className='text-sm font-semibold'>Firdovsi</div>
+  )
+}
+
+export default UserName
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import SearchQuery from '../features/order/SearchQuery'
+import UserName from '../features/user/UserName'
 
 function Header() {
     return (
-        <header className='bg-yellow-500'>
-            <Link to={'/'}>Fast React Pizza Co.</Link>
+        <header className='bg-yellow-500 uppercase'>
+            <Link className='tracking-widest' to={'/'}>Fast React Pizza Co.</Link>
             <p>Jonas</p>
             <SearchQuery />
+            <UserName />
         </header>
     )
 }
